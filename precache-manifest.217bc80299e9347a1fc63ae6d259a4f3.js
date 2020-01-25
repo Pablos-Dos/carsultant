a9be44062707ff8267349fc634d3840b00d30e76
Pablos-Dos <paulkluge@yahoo.de>
2020-01-25 18:52:10 +0100

    Updates

---
+++ precache-manifest.217bc80299e9347a1fc63ae6d259a4f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec3ff71885c5c99f1df5f64afdc7146",
+    "url": "/carsultant/index.html"
+  },
+  {
+    "revision": "bd6122a65faf05b35408",
+    "url": "/carsultant/static/css/main.145e8c7e.chunk.css"
+  },
+  {
+    "revision": "918f115fbbabee9ad1ea",
+    "url": "/carsultant/static/js/2.7a18c7a1.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/carsultant/static/js/2.7a18c7a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd6122a65faf05b35408",
+    "url": "/carsultant/static/js/main.ad7abfb8.chunk.js"
+  },
+  {
+    "revision": "eced8701092ac3106e10",
+    "url": "/carsultant/static/js/runtime-main.e91bab65.js"
+  }
+]);
